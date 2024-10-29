b4426e42fa820ecdf173c78970aabb74cb83b5e3
Svyatoos <138365967+Svyatoos@users.noreply.github.com>
2024-10-29 22:55:34 +1200

    Update main.py
    это твой код ля работы
    свой код пиши в между коминтариев

--- ART/main.py
+++ ART/main.py
@@ -1,6 +1,52 @@
 # подключаем библиотеки
 import cv2
-from deepface import DeepFace
+#import matplotlib.pyplot as plt
+
+
+# функция определения лиц
+def highlightFace(net, frame, conf_threshold=0.7):
+    # делаем копию текущего кадра
+    frameOpencvDnn=frame.copy()
+    # высота и ширина кадра
+    frameHeight=frameOpencvDnn.shape[0]
+    frameWidth=frameOpencvDnn.shape[1]
+    # преобразуем картинку в двоичный пиксельный объект
+    blob=cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+    # устанавливаем этот объект как входной параметр для нейросети
+    net.setInput(blob)
+    # выполняем прямой проход для распознавания лиц
+    detections=net.forward()
+    # переменная для рамок вокруг лица
+    faceBoxes=[]
+    # перебираем все блоки после распознавания
+    for i in range(detections.shape[2]):
+        # получаем результат вычислений для очередного элемента
+        confidence=detections[0,0,i,2]
+        # если результат превышает порог срабатывания — это лицо
+        if confidence>conf_threshold:
+            # формируем координаты рамки
+            x1=int(detections[0,0,i,3]*frameWidth)
+            y1=int(detections[0,0,i,4]*frameHeight)
+            x2=int(detections[0,0,i,5]*frameWidth)
+            y2=int(detections[0,0,i,6]*frameHeight)
+            # добавляем их в общую переменную
+            faceBoxes.append([x1,y1,x2,y2])
+            
+            
+            # рисуем рамку на кадре
+            #cv2.rectangle(frameOpencvDnn, (x1,y1), (x2,y2), (0,255,0), int(round(frameHeight/150)), 8)
+
+    # возвращаем кадр с рамками
+    
+    return frameOpencvDnn,faceBoxes
+
+# загружаем веса для распознавания лиц
+faceProto="opencv_face_detector.pbtxt"
+# и конфигурацию самой нейросети — слои и связи нейронов
+faceModel="opencv_face_detector_uint8.pb"
+
+# запускаем нейросеть по распознаванию лиц
+faceNet=cv2.dnn.readNet(faceModel,faceProto)
 
 # получаем видео с камеры
 video=cv2.VideoCapture(0)
@@ -13,13 +59,37 @@ while cv2.waitKey(1)<0:
         # останавливаемся и выходим из цикла
         cv2.waitKey()
         break
-    
-    
+    # распознаём лица в кадре
+    resultImg,faceBoxes=highlightFace(faceNet,frame)
 
-    # выводим картинку с камеры
-    
-    cv2.imshow("Face detection", frame)
+    # если лиц нет
+    if not faceBoxes:
+        # выводим в консоли, что лицо не найдено
+        print("Лица не распознаны")
 
+    i=0
+    imageFace = resultImg[faceBoxes[i][1]-10 : faceBoxes[i][3]+10, faceBoxes[i][0]-10 : faceBoxes[i][2]+10]
+    
+    #    imageFace это переменная с изоброжением 
+    #    там горантировоно тока лицо
+    #    тута твой код
+    
+    cv2.imshow("Face detection", imageFace) #это вывод изоброжения литца    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    #
+    #
+    #
+    
+   
 
 
 
