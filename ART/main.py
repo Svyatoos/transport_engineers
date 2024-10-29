f3ff437585369eb73c099f9096c89f833887b1a4
Svyatoos <138365967+Svyatoos@users.noreply.github.com>
2024-10-29 22:37:51 +1200

    Create main.py
    For ART

---
+++ ART/main.py
@@ -0,0 +1,26 @@
+# подключаем библиотеки
+import cv2
+from deepface import DeepFace
+
+# получаем видео с камеры
+video=cv2.VideoCapture(0)
+# пока не нажата любая клавиша — выполняем цикл
+while cv2.waitKey(1)<0:
+    # получаем очередной кадр с камеры
+    hasFrame,frame=video.read()
+    # если кадра нет
+    if not hasFrame:
+        # останавливаемся и выходим из цикла
+        cv2.waitKey()
+        break
+    
+    
+
+    # выводим картинку с камеры
+    
+    cv2.imshow("Face detection", frame)
+
+
+
+
+
